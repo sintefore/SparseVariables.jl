985ad7414ad42d90768118447b72032fde79bf24
Truls Flatberg <Truls.Flatberg@sintef.no>
2021-09-08 14:15:13 +0200

    Improve tests and remove \star as wildcard

--- src/JuMPUtils.jl
+++ src/JuMPUtils.jl
@@ -11,7 +11,7 @@ include("tables.jl")
 
 export @sparsevariable
 export insertvar!
-export ⋆
+export SolutionTable
 
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -21,11 +21,8 @@ car_cost = JU.SparseArray(Dict(
     ))
 
 @testset "Select" begin
-    @test JU.select(indices, (⋆, 1957)) == [lotus]
     @test JU.select(indices, (:, 1957)) == [lotus]
-    @test JU.select(indices, (⋆, <(2000) )) == [lotus]
     @test JU.select(indices, (:, <(2000) )) == [lotus]
-    @test JU.select(indices, (⋆, <(2000)), (2,1)) == [lotus]
     @test JU.select(indices, (:, <(2000)), (2,1)) == [lotus]
     ntnames = (car=1, year=2)
     @test JU.select(indices, (;car="lotus"), ntnames) == [lotus]
@@ -33,7 +30,7 @@ car_cost = JU.SparseArray(Dict(
     @test JU.select(indices, (year=1957, car="lotus"), ntnames) == [lotus]
 
     isenglish(x) = x in["lotus","aston martin"]
-    @test JU.select(indices, (isenglish, ⋆)) == [lotus]
+    @test JU.select(indices, (isenglish, :)) == [lotus]
 
     complex_query(c,y) = c in ["opel","bmw"] && ((y > 2002) || (y <= 2000))
     complex_query(x) = complex_query(x...) 
@@ -89,7 +86,7 @@ end
     y = JU.create_variables_dictionary3(m, 2, "y", indices)
     @test typeof(y) == Dictionary{Tuple{String, Int64}, VariableRef}
 
-    a = @constraint(m, sum(JU.select(y, ("lotus", ⋆))) <=1)
+    a = @constraint(m, sum(JU.select(y, ("lotus", :))) <=1)
     @test typeof(a) == ConstraintRef{Model, MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}, ScalarShape}
 end
 
@@ -121,7 +118,7 @@ end
     @constraint(m, con1, sum(y[c,i] + z[c,i] for c in cars, i in year) <= 300)
     @test length(constraint_object(con1).func.terms) == 5
 
-    @constraint(m, con2[i in year], sum(car_cost[c,i] * y[c,i] for (c,i) in JU.select(y, ⋆, i)) <= 300)
+    @constraint(m, con2[i in year], sum(car_cost[c,i] * y[c,i] for (c,i) in JU.select(y, :, i)) <= 300)
     @test length(constraint_object(con2[2001]).func.terms) == 2
     
     @objective(m, Max, sum(z[c,i] + 2y[c,i] for c in cars, i in year))
@@ -140,13 +137,13 @@ end
 
 @testset "Tables" begin
     m = Model()
-    @sparsevariable(m, y[c,i] for (c,i) in keys(car_cost))
+    @sparsevariable(m, y[car,year] for (car,year) in keys(car_cost))
     @sparsevariable(m, z[car,year])
     for c in ["opel", "tesla", "nikola"]
         insertvar!(z, c, 2002)
     end
     @constraint(m, con1, sum(y[c,i] + z[c,i] for c in cars, i in year) <= 300)
-    @constraint(m, con2[i in year], sum(car_cost[c,i] * y[c,i] for (c,i) in JU.select(y, ⋆, i)) <= 300)
+    @constraint(m, con2[i in year], sum(car_cost[c,i] * y[c,i] for (c,i) in JU.select(y, :, i)) <= 300)
 
     @objective(m, Max, sum(z[c,i] + 2y[c,i] for c in cars, i in year))
 
@@ -154,8 +151,11 @@ end
     optimize!(m)
 
     t = JU.SolutionTable(y)
+    @test typeof(t) == JU.SolutionTable
+    @test length(t) == 4
 
-    r = JU.SolutionRow(("bmw",2001), 3.2, JU.names(t))
-    
+    r = first(t)
+    @test typeof(r) == JU.SolutionRow
+    @test r.car == "bmw"
 end
  
